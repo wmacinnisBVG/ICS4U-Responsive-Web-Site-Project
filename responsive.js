e55f2b25b35e7d7f0c8eeec1feb454121386476a
William MacInnis <will@macinnis.xyz>
2022-02-11 20:32:30 -0500

    Final submit

---
+++ responsive.js
@@ -0,0 +1,13 @@
+var x = document.getElementById("myTopnav");
+
+function openMenu() {
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+function triggerMenu(){
+    x.style.display = "block";
+
+}
